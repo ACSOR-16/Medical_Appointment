0aae320b24efcf966332ed7de7b1df7338152238
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2024-05-30 12:04:17 -0500

    patient clsss

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,12 @@ public class Main {
         Doctor myDoctor = new Doctor("Jean Cris", "Cardiology");
         System.out.println(myDoctor.name);
         System.out.println(myDoctor.speciality);
+
+        Patient patient = new Patient("Alejandro", "alejandro@gmail.com");
+        System.out.println(patient.name);
+        System.out.println(patient.address);
+
+        patient.weight = 70.85;
+        patient.height = 178;
     }
 }
