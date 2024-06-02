9cfed00f6cd1f2a2982312033bd7f657dae6c565
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2024-06-01 23:34:21 -0500

    enum test - main class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,26 @@ import java.util.Date;
 import static UI.UIMenu.*;
 
 public class Main {
+
+    public enum Day{
+        MONDAY("Lunes"),
+        TUESDAY("Martes"),
+        WEDNESDAY("Miercoles"),
+        THURSDAY("Jueves"),
+        FRIDAY("Viernes"),
+        SATURDAY("Sabado"),
+        SUNDAY("Domingo");
+
+        private String spanish;
+        private Day(String day) {
+            spanish = day;
+        }
+
+        public String getSpanish() {
+            return spanish;
+        }
+    }
+
     public static void main(String[] args) {
 //        showMenu();
 
@@ -15,7 +35,10 @@ public class Main {
         for (Doctor.AvailableAppointment availableAppointment: myDoctor.getAvailableAppointments()) {
             System.out.println(availableAppointment.getDate() + " " + availableAppointment.getTime());
         }
-        
+
+        System.out.println(Day.FRIDAY.spanish);
+        System.out.println(Day.SATURDAY.getSpanish());
+        System.out.println(Day.SUNDAY);
         /*System.out.println(myDoctor.getName());
         System.out.println(myDoctor.getSpeciality());
 
