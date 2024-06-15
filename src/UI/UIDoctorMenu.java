3f8b8e5026b9237b171d58191bd6a204576d10fd
ACSOR-16 <oscar.aguirre.tarazona@gmail.com>
2024-06-15 14:17:07 -0500

    UI docotr classs menu

---
+++ src/UI/UIDoctorMenu.java
@@ -0,0 +1,60 @@
+package UI;
+
+import java.util.Scanner;
+
+public class UIDoctorMenu {
+    public static void showDoctorMenu() {
+        int response = 0;
+
+        do {
+            System.out.println("\n\n Doctor");
+            System.out.println("Welcome " + UIMenu.doctorLogged.getName());
+            System.out.println("1. Add available appointment");
+            System.out.println("2. My scheduled appointment");
+            System.out.println("3. Log out");
+
+            Scanner scanner = new Scanner(System.in);
+            response = Integer.valueOf(scanner.nextLine());
+
+            switch (response) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 0:
+                    UIMenu.showMenu();
+                    break;
+            }
+        } while (response != 0);
+    }
+
+    private static void showAddAvailableAppointmentsMenu() {
+        int response = 0;
+
+        do {
+            System.out.println("\nAdd Available Appointment");
+            System.out.println("Select a Month");
+
+            for (int i = 0; i < 3; i++) {
+                int index = i + 1;
+                System.out.println(index + "." + UIMenu.MONTHS[i]);
+            }
+
+            System.out.println("0. Return");
+
+            Scanner scanner = new Scanner(System.in);
+            response = Integer.valueOf(scanner.nextLine());
+
+            if (response > 0 && response < 4) {
+                int monthSelected = response;
+                System.out.println(monthSelected + " " + UIMenu.MONTHS[monthSelected]);
+                System.out.println("insert the date available: [dd/mm/yyyy]");
+
+                String dateSelected = scanner.nextLine();
+                System.out.println("Your date is: " + dateSelected + "\n1. Correct \n2. Change date");
+            } else if (response == 0) {
+                showDoctorMenu();
+            }
+        } while (response != 0);
+    }
+}
